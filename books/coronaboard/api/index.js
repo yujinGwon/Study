f9dd76320af79c26a2f45dd86bb34829cd96463f
newJin <newjin@newJinui-iMac.local>
2022-09-22 22:30:01 +0900

    2.6.3 controller & routing setting

--- books/coronaboard/api/index.js
+++ books/coronaboard/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
+const globalStatController = require('./controller/global-stat.controller');
 
 async function launchServer() {
     const app = express();
@@ -9,6 +10,10 @@ async function launchServer() {
     app.get('/', (req, res) => {
         res.json({ message: 'Hello CoronaBoard!' });
     });
+
+    app.get('/global-stats', globalStatController.getAll);
+    app.post('/global-stats', globalStatController.insertOrUpdate);
+    app.delete('/global-stats', globalStatController.remove);
 
     try {
         await sequelize.sync();
