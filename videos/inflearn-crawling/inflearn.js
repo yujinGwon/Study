b162ba632a4ad7a7e977e4ec63a2d83a064e354a
yujinGwon <yjgwon@seoul-soft.com>
2022-09-22 12:22:56 +0900

    inflearn-crawling(node.js)

---
+++ videos/inflearn-crawling/inflearn.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require('cheerio');
+
+const getHTML = async(keyword) => {
+    try {
+        return await axios.get("https://www.inflearn.com/courses?s=" + encodeURI(keyword));
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+const parsing = async (keyword) => {
+    const html = await getHTML(keyword);
+    //console.log(html);
+    const $ = cheerio.load(html.data);
+    const $courseList = $(".course_card_item");
+
+    let courses = [];
+    $courseList.each((idx, node) => {
+        const title = $(node).find(".course_title").text();
+        //console.log(title);
+        courses.push({
+            title: $(node).find(".course_title:eq(0)").text(),
+            instructor: $(node).find(".instructor").text(),
+            price: $(node).find(".price").text(),
+            rating: $(node).find(".star_solid").css("width"),
+            img: $(node).find(".card-image > figure > img").attr("src")
+        })
+    });
+
+    console.log(courses);
+}
+
+parsing("자바스크립트");
