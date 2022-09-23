bb77b083a47400f1d3cb17e360f771b193828ffa
newJin <newjin@newJinui-iMac.local>
2022-09-23 21:37:41 +0900

    5.3.3 css design

--- books/coronaboard/web/src/components/slide.js
+++ books/coronaboard/web/src/components/slide.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { css } from '@emotion/react';
 
 export function Slide(props) { // Slide라는 이름의 함수형 컴포넌트 선언
     const { title, children } = props; // 부모 컴포넌트에서 전달받은 속성값 추출
     return ( // 이 컴포넌트가 랜더링될 형태 반환
-        <div>
+        <div
+          css = {css`
+          text-align: center;
+          border-top: 1px solid #aaa;
+          padding-top: 40px;
+          padding-bottom: 60px;
+          `}
+        >
             <h2>{title}</h2>
             <div>{children}</div>
         </div>
