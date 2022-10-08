d6fd73fa172dc969319a4a8581673fcf7133dcf7
yujinGwon <yjgwon@seoul-soft.com>
2022-10-08 18:44:23 +0900

    6.1 useEffect

--- nomadcoders/React_Track/movieWeb/react-for-beginners2/src/App.js
+++ nomadcoders/React_Track/movieWeb/react-for-beginners2/src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....")
+  }, []);
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
