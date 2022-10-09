dd06b0724e3bebad41c0fbbf017390157e053cc3
yujinGwon <yjgwon@seoul-soft.com>
2022-10-09 19:13:22 +0900

    7.5 React Router

--- nomadcoders/React_Track/movieWeb/react-for-beginners2/src/App.js
+++ nomadcoders/React_Track/movieWeb/react-for-beginners2/src/App.js
@@ -1,6 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
