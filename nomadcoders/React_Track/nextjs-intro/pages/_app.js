cb09450092e5260c2204c2924870d6d5d79342ea
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-11 14:49:38 +0900

    1.6 Custom App

---
+++ nomadcoders/React_Track/nextjs-intro/pages/_app.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function App({Component, pageProps}) {
+    return (
+        <div>
+            <NavBar />
+            <Component {...pageProps} />
+            <span>hello</span>
+            <style jsx global>{`
+                a {
+                    color: white;
+                }
+                `}</style>
+        </div>
+    );
+}
