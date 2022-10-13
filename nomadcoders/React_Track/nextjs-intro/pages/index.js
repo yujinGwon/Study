8fd21967bb92e3f5f9dacf44327ba498a690586c
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-13 18:57:16 +0900

    cursor: pointer

--- nomadcoders/React_Track/nextjs-intro/pages/index.js
+++ nomadcoders/React_Track/nextjs-intro/pages/index.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home({results}) {
+export default function Home({ results }) {
 
-    return (
-        <div className="container">
-            <Seo title="Home" />
+  return (
+    <div className="container">
+      <Seo title="Home" />
 
-            {results?.map((movie) => (
-                <div className="movie" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    <h4>{movie.original_title}</h4>
-                </div>
-            ))}
-            <style jsx>{`
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
         .container {
           display: grid;
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -34,15 +37,15 @@ export default function Home({results}) {
           text-align: center;
         }
       `}</style>
-        </div>
-    );
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-    const { results } = await (await fetch(`http://127.0.0.1:3000/api/movies`)).json();
-    return {
-        props: {
-            results,
-        },
-    };
+  const { results } = await (await fetch(`http://127.0.0.1:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
