dfbfe1075dfca937d2a0b2777cb4bd5cd05bf1ec
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-13 17:25:36 +0900

    2.2 Redirect and Rewrite

--- nomadcoders/React_Track/nextjs-intro/next.config.js
+++ nomadcoders/React_Track/nextjs-intro/next.config.js
@@ -1,7 +1,31 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+      {
+        source: "/abc/:path*",
+        destination: "/efghijk/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      }
+    ];
+  },
+};
 
 module.exports = nextConfig
