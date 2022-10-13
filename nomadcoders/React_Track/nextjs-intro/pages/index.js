ba5f8f6829b559a9b8337f398ea07b1d930dc9d0
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-13 16:52:26 +0900

    2.1 Fetching Data

--- nomadcoders/React_Track/nextjs-intro/pages/index.js
+++ nomadcoders/React_Track/nextjs-intro/pages/index.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "";
+
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const { results } = await (
+                await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+                )).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <div>
             <Seo title="Home" />
-            <h1 className="active">Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map(movie => <div key={movie.id}>
+                <h4>{movie.original_title}</h4>
+            </div>)}
+
         </div>
     );
 }
