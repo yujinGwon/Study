4dc7930d1dc33e8dd2fa8d7418a12dfb722a8725
yujinGwon <gwonyj36@gmail.com>
2022-10-15 01:29:48 +0900

    2.6 Movie Detail

--- nomadcoders/React_Track/nextjs-intro/next.config.js
+++ nomadcoders/React_Track/nextjs-intro/next.config.js
@@ -22,9 +22,13 @@ const nextConfig = {
     return [
       {
         source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-      }
-    ];
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`
+      },
+    ]; 
   },
 };
 
--- nomadcoders/React_Track/nextjs-intro/pages/movies/[id].js
+++ nomadcoders/React_Track/nextjs-intro/pages/movies/[id].js
@@ -2,7 +2,9 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
     const router = useRouter();
-    console.log("이걸 네번이나??");
-    console.log(router);
-    return "detail";
+    return (
+        <div>
+            <h4>{router.query.title || "Loading"}</h4>
+        </div>
+    );
 }
