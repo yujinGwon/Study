c2567a28dce69085a1b01f3269218abd09c8ad59
yujinGwon <yjgwon@seoul-soft.com>
2022-10-16 20:53:30 +0900

    2.7 Catch All

--- nomadcoders/React_Track/nextjs-intro/pages/index.js
+++ nomadcoders/React_Track/nextjs-intro/pages/index.js
@@ -5,27 +5,21 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push({
-      pathname: `/movies/${id}`,
-      query: {
-        title,
-      },
-    }, `/movies/${id}`);
-  }
+    router.push(`/movies/${title}/${id}`);
+  };
 
   return (
     <div className="container">
       <Seo title="Home" />
 
       {results?.map((movie) => (
-        <div onClick={()=>onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <Link href={{
-                  pathname: `/movies/${movie.id}`,
-                  query: {
-                    title: movie.original_title,
-                  },
-          }} as={`movies/${movie.id}`} >
+          <Link href={`/movies/${movie.original_title}/${movie.id}`}>
             <a>{movie.original_title}</a>
           </Link>
         </div>
@@ -59,7 +53,9 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  const { results } = await (await fetch(`http://127.0.0.1:3000/api/movies`)).json();
+  const { results } = await (
+    await fetch(`http://127.0.0.1:3000/api/movies`)
+  ).json();
   return {
     props: {
       results,
