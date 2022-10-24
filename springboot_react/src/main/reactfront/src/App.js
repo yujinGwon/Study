e37a09222be9cc684e5f518cdab6a9ba492edd30
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-24 12:25:55 +0900

    number recording app

--- springboot_react/src/main/reactfront/src/App.js
+++ springboot_react/src/main/reactfront/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //let name = 'newJin';
+  const [num, setNum] = useState(0);
+
+  const [numList, setNumList] = useState([]);
+
+  function numRecording () {
+    setNumList([...numList, num]);
+    setNum(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>현재 숫자 : {num}</div>
+      <button onClick={() => {setNum(num+1)}}>숫자 증가</button>
+      <button onClick={() => {setNum(num-1)}}>숫자 감소</button>
+      <button onClick={numRecording}>숫자 기록</button>
+      <h1>저장된 숫자</h1>
+      <ul>
+        {numList.map((num) => (
+          <li>{num}</li>
+        ))}
+      </ul>
     </div>
   );
 }
