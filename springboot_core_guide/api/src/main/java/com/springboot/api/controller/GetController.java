7179e823ed66308cc5a9521bbd1b49b9f95d46c0
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2022-10-27 15:17:43 +0900

    Swagger로 REST API 명세를 문서화 하기

--- springboot_core_guide/api/src/main/java/com/springboot/api/controller/GetController.java
+++ springboot_core_guide/api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -34,11 +36,12 @@ public class GetController {
     }
 
     // http://localhost:8080/api/v1/get-api/request1?name=value1&eamil=value2&organization=value3
+    @ApiOperation(value = "GET 메서드 예제", notes = "@RequestParam을 활용한 GET Method")
     @GetMapping(value = "/request1")
     public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization) {
+            @ApiParam(value = "이름", required = true) @RequestParam String name,
+            @ApiParam(value = "이메일", required = true) @RequestParam String email,
+            @ApiParam(value = "회사", required = true) @RequestParam String organization) {
         return name + " " + email + " " + organization;
     }
 
